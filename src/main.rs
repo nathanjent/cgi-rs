4e99f11748396989726f2bb8975ecbadceb594d4
nathanjent <nathanjent@gmail.com>
2017-03-08 22:48:29 -0500

    Add read from stdin.
    Update error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use std::io::{self, Read, Write};
+use std::env;
+
 fn main() {
+    let content_length = env::var("CONTENT_LENGTH").unwrap_or("0".into())
+        .parse::<u64>().expect("Error parsing CONTENT_LENGTH");
+    let status = match handle(content_length) {
+        Ok(_) => 0,
+        Err(_) => 1,
+    };
+    ::std::process::exit(status);
+}
+
+fn handle(content_length: u64) -> io::Result<()> {
+    let mut buffer = Vec::new();
+    io::stdin().take(content_length).read_to_end(&mut buffer)?;
+
     println!("Content-Type: text/html");
     println!();
     println!("<p>Hello, world!</p>");
@@ -8,5 +24,9 @@ fn main() {
     }
     println!("</ul>");
 
-    ::std::process::exit(0);
+    println!("<p>");
+    io::stdout().write(&buffer[..])?;
+    println!("</p>");
+
+    Ok(())
 }
